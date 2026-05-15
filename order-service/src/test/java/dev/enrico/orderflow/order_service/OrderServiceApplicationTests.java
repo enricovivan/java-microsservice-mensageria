58ce6225a0a3861a5ea87b2ffe907fcdd646afd6
Enrico Vivan <enricovivan21@gmail.com>
2026-05-14 21:27:00 -0300

    primeiro upload dos services

---
+++ order-service/src/test/java/dev/enrico/orderflow/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enrico.orderflow.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
